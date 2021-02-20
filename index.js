e52e7eff2f3d456022f3a88211e1023313456faf
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-02-20 11:11:10 -0700

    added a new folder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const inquirer
-const generateReadMe
+const inquirer = require("inquirer")
+const generatorMarkdown = require("./demoReadMe")
 const fs = require("fs")
 
 const dataInput = [
@@ -9,54 +9,63 @@ const dataInput = [
         name: 'username',
       },
 
-    {
-        type: 'input',
-        message: 'Please enter your email address?',
-        name: 'email',
-      },
+    // {
+    //     type: 'input',
+    //     message: 'Please enter your email address?',
+    //     name: 'email',
+    //   },
     
-      {
-        type: 'input',
-        message: 'Please enter the name of your project?',
-        name: 'title',
-      },   
+    //   {
+    //     type: 'input',
+    //     message: 'Please enter the name of your project?',
+    //     name: 'title',
+    //   },   
     
-      {
-        type: 'input',
-        message: 'Please provide a description of your project?',
-        name: 'description',
-      },
+    //   {
+    //     type: 'input',
+    //     message: 'Please provide a description of your project?',
+    //     name: 'description',
+    //   },
     
-      {
-        type: 'input',
-        message: 'What must be installed to run this program?',
-        name: 'installation',
-      },
+    //   {
+    //     type: 'input',
+    //     message: 'What must be installed to run this program?',
+    //     name: 'installation',
+    //   },
     
-      {
-        type: 'input',
-        message: 'Please enter licenses needed?',
-        name: 'license',
-      },
+    //   {
+    //     type: 'input',
+    //     message: 'Please enter licenses needed?',
+    //     name: 'license',
+    //   },
     
-      {
-        type: 'input',
-        message: 'Please add contributors',
-        name: 'contributors',
-      },
+    //   {
+    //     type: 'input',
+    //     message: 'Please add contributors',
+    //     name: 'contributors',
+    //   },
       
-    {
-        type: 'input',
-        message: 'Please add contributors',
-        name: 'contributors',
-      },  
+    // {
+    //     type: 'input',
+    //     message: 'Please add contributors',
+    //     name: 'contributors',
+    //   },  
 ];
 
-newFile = (fileName, info) => {
-    fs.writeFile("./readMeDemo/"+fileName, info, function(err){
+newFile = (fileName, data) => {
+    fs.writeFile("./readMeDemo/"+ fileName, data, function(err) {
         if (err) {
             return console.log(err);
         }
-        console.log(fileName + "Successfully written!")
+        console.log(fileName + "Successfully written!");
+    });
+};
+
+initialize = () => {
+    inquirer.prompt(dataInput)
+    .then (function(data) {
+        newFile("readMeDemo.md", generatorMarkdown(data))
     })
 }
+
+initialize()
